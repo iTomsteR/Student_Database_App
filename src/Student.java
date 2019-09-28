2d5c311597da36f027ff53bd55e665fe054393ba
iTomster <tomsterman97@gmail.com>
2019-09-28 13:29:45 +0300

    Create basic outline for the project.

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,23 @@
 public class Student {
 
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private final int COURSE_COST = 600;
+
+    //Constructor: prompt user to enter name and year of student.
+
+    //Generate Student ID
+
+    //Enroll in courses
+
+    //View balance
+
+    //Pay tuition
+
+    //Show status
 
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,7 +1,11 @@
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
-        
+
+        //Ask how many students we want to add.
+
+        //Create n number of students.
+
     }
 
 }
