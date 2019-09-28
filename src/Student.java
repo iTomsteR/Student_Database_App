c6f3698ad4e92ce9c6477c1dc5ab48c2c2f9a7bb
iTomster <tomsterman97@gmail.com>
2019-09-28 13:50:19 +0300

    Generate student ID.

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,39 @@
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
-    private int studentID;
+    private int gradeYear;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private final int COURSE_COST = 600;
+    private final static int COURSE_COST = 600;
+    private static int id = 1000;
 
     //Constructor: prompt user to enter name and year of student.
+    public Student() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("Choose student class level:\n1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nChoose student class level: ");
+        this.gradeYear = in.nextInt();
+
+        generateStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+    }
 
     //Generate Student ID
+    private void generateStudentID() {
+        //Grade level + ID
+        id++;
+        studentID = gradeYear + "" + id;
+    }
 
     //Enroll in courses
 
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 
+        Student student1 = new Student();
         //Ask how many students we want to add.
 
         //Create n number of students.
